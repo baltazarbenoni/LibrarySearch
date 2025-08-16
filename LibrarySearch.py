2714f01c19bcee11279b02f50e339bf10080df7d
unknown <daniel.snapir@gmail.com>
2025-08-16 17:59:14 +0300

    finished library search app!

--- LibrarySearch.py
+++ LibrarySearch.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import webbrowser
 import re
 
 class Main:
@@ -8,34 +9,96 @@ class Main:
     h = Path.home()
     source = Path(h / "Desktop" / "Livres")
     foundFiles = list()
+    searchKey = ""
 
-    def fileSearch(self, key):
+    def fileSearch(self):
         contents = os.listdir(self.source)
-        if(contents.count == 0):
+        if(len(contents) == 0):
             print("The library folder is empty!\nCannot conduct search")
             return
         else:
+            amount = str(len(contents))
+            print("\nSearching the pattern " + self.searchKey + "\nFrom " + amount + " files.\n")
             for x in contents:
                 if (os.path.isfile(Path(self.source / x)) == False):
                     continue
                 else:
-                    self.checkFile(x, key)
+                    self.checkFile(x, self.searchKey)
 
     def checkFile(self, item, key):
-        regex = "(" + key + ")$"
+        regex = "(" + key + ")"
         result = re.search(regex, item)
         #IF FILE MATCHES SEARCH, ADD IT TO LIST
         if(result != None):
             self.foundFiles.append(item)
-            #IF ONLY ONE FILE FOUND, OPEN FILE?
-            #OR OPEN FILE RIGHT AWAY?
+    
+    def printResults(self):
+        if(len(self.foundFiles) == 0):
+            return False
+        for x in self.foundFiles:
+            a = str(self.foundFiles.index(x))
+            print(a + ": " + x + "\n")
+        return True
+    
+    def openFile(self, input):
+        a = int(input)
+        file = self.foundFiles[a]
+        path = Path(self.source / file)
+        print("opening file: " + str(path))
+        webbrowser.open_new(path)
+
+
+    def checkInput(self, input):
+        #CHECK IF VALUE IS INT
+        try:
+            val = int(input)
+            if(val < 0):
+                print("Given index less than zero, try again.\n")
+                return 0
+            elif(val >= len(self.foundFiles)):
+                print("Given index exceeds the amount of results. Try again\n")
+                return 0
+            else:
+                return 2 
+        except ValueError:
+            print("Input not an integer. Conducting a new search with given variable.")
+            return 1 
 
 #INIT APP
-print("Welcome to your library search app!")
+print("Welcome to your library search app!\n")
 instance = Main()
-key = input("Type what you want to search from your library")
-if(key == 'or'):
-    instance.organizefiles()
-
-
-
+#GET THE FIRST PROMPT
+key = input("Type what you want to search from your library\n")
+instance.searchKey = key
+#RUN THIS LOOP WHILE APP IS ACTIVE
+active = True
+while active: 
+    #FIRST FILE SEARCH
+    instance.fileSearch()
+    found = instance.printResults()
+    if(not found):
+        key = input("No files found! Enter a new search!\n")
+        instance.searchKey = key
+        continue
+    #THE FILES FOUND ARE PRINTED. ASK IF USER WANTS TO OPEN ONE OF THEM.
+    print("If you wish to open one of the files, enter its index to open it. Else, conduct a new search by typing something else.\n")
+    #VARIABLE 'NEXTPROCESS' INDICATES WHETHER NEXT PROCESS IS SELECTED.
+    # 0 MEANS NOT SELECTED OR INCORRECT INPUT
+    # 1 MEANS STRING --> CONDUCT A NEW SEARCH WITH GIVEN PROMPT.
+    # 2 MEANS INTEGER --> OPEN FILE WITH GIVEN INDEX
+    nextProcess = 0 
+    index = 0
+    #RUN THIS LOOP WHILE NEXT PROCESS IS NOT SELECTED.
+    while nextProcess < 1:
+        userInput = input()
+        nextProcess = instance.checkInput(userInput)
+        index = userInput
+    #CONTINUE TO NEW SEARCH.
+    if(nextProcess < 2):
+        print("User input: " + index)
+        instance.searchKey = index
+    #OPEN THE FILE
+    elif(nextProcess < 3):
+        instance.openFile(index)
+        active = False
+print("See you next time!")
